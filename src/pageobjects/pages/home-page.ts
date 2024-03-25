b8fcdf72b88855de96968de3dd0962860a0635c6
facundorosonovich <facu.rosonovich@gmail.com>
2024-03-25 11:14:38 +0100

    Adding second UI test

--- src/pageobjects/pages/home-page.ts
+++ src/pageobjects/pages/home-page.ts
@@ -5,12 +5,14 @@ import {Header} from "../components/header";
 export class HomePage {
     private readonly page: Page
     private readonly headerContainer: Locator
+    readonly addTaskButton: Locator
     header: Header
 
     constructor(page: Page) {
         this.page = page
         // Locators
         this.headerContainer = this.page.getByRole('navigation')
+        this.addTaskButton = this.page.getByRole('button', { name: '+ Add Task' })
 
         // Components
         this.header = new Header(this.headerContainer)
--- tests/ui/ui-tests.spec.ts
+++ tests/ui/ui-tests.spec.ts
@@ -18,8 +18,24 @@ test.describe('Task Management UI tests', () => {
 
         // Action - Logout
         await homePage.header.logout()
-
-
-
     })
+
+    test('When logged in I can create a task', async ({page}) => {
+
+        // Arrange
+        const homePage = new HomePage(page);
+
+        // Actions - Navigate to Home Page
+        await homePage.goto();
+
+        // Assert 1 - I can't add a task when not logged in
+        await expect(homePage.addTaskButton).toBeHidden();
+
+        // Actions - Login
+        await homePage.header.login(username, password);
+
+        // Assert 2 - I can add a task when logged in
+        await expect(homePage.addTaskButton).toBeVisible()
+    })
+
 })
